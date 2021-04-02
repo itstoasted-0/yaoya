2f8f348b384114825db31e8fbfa92b28a7eaa5ac
itstoasted-0 <snow.w686@gmail.com>
2021-04-02 17:17:09 +0900

    クラスを使った商品の表示

--- greengrocer_main.rb
+++ greengrocer_main.rb
@@ -11,7 +11,12 @@ end
 class Greengrocer
   def initialize(product_params)
     @products = []
-    product_params.each do |param|
+    register_product(product_params)
+  end
+
+  #商品の追加
+  def register_product(adding_product_params)
+    adding_product_params.each do |param|
       @products << Product.new(param)
     end
   end
@@ -24,8 +29,6 @@ class Greengrocer
   end
 end
 
-
-
 # 商品データ
 product_params1 = [
   {name: "トマト", price: 100},
@@ -34,16 +37,17 @@ product_params1 = [
   {name: "なす", price: 400}
 ]
 
-product_params2 = [
-  {name: "パセリ", price: 100},
-  {name: "きゅうり", price: 200}
-]
-
-
-
 # 八百屋の開店
 greengrocer1 = Greengrocer.new(product_params1)
-greengrocer2 = Greengrocer.new(product_params2)
+
+# 追加商品データadding_product_params1を定義
+adding_product_params1 = [
+  {name: "ごぼう", price: 250},
+  {name: "れんこん", price: 350}
+]
+
+# 商品を登録するメソッドを呼び出す（★ここを追加★）
+greengrocer1.register_product(adding_product_params1)
+
 # 商品を表示
 greengrocer1.disp_products
-greengrocer2.disp_products
