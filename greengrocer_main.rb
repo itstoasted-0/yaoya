00167c16dd2445d9cdba8b6622b0901b3f63eaa2
itstoasted-0 <snow.w686@gmail.com>
2021-04-02 09:41:43 +0900

    init

---
+++ greengrocer_main.rb
@@ -0,0 +1,14 @@
+require "./methods.rb"
+
+# 商品データ
+products = [
+  {name: "トマト", price: 100},
+  {name: "きゅうり", price: 200},
+  {name: "玉ねぎ", price: 300},
+  {name: "なす", price: 400}
+]
+
+disp_products(products) # 商品を表示
+chosen_product = choose_product(products) # 商品を選択
+quantity_of_product = decide_quantity(chosen_product) # 商品の個数を決定
+calculate_charges(chosen_product, quantity_of_product) # 合計金額を計算
