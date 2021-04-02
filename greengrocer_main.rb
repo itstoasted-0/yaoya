cce9dbc7ead9b0913f15993e79806a0b0dfd62b9
itstoasted-0 <snow.w686@gmail.com>
2021-04-02 15:29:23 +0900

    ２-１. 商品を表示する（４）商品の番号が自動で振られるようにする

--- greengrocer_main.rb
+++ greengrocer_main.rb
@@ -1,14 +1,49 @@
-require "./methods.rb"
+class Product
+  attr_reader :id, :name, :price
+  @@count = 30
+  def initialize(product_params)
+    @id = @@count += 1
+    @name = product_params[:name]
+    @price = product_params[:price]
+  end
+end
+
+class Greengrocer
+  def initialize(product_params)
+    @products = []
+    product_params.each do |param|
+      @products << Product.new(param)
+    end
+  end
+
+  def disp_products
+    puts "いらっしゃいませ！商品を選んで下さい。"
+    @products.each do |product|
+      puts "#{product.id}.#{product.name}(#{product.price}円)"
+    end
+  end
+end
+
+
 
 # 商品データ
-products = [
+product_params1 = [
   {name: "トマト", price: 100},
   {name: "きゅうり", price: 200},
   {name: "玉ねぎ", price: 300},
   {name: "なす", price: 400}
 ]
 
-disp_products(products) # 商品を表示
-chosen_product = choose_product(products) # 商品を選択
-quantity_of_product = decide_quantity(chosen_product) # 商品の個数を決定
-calculate_charges(chosen_product, quantity_of_product) # 合計金額を計算
+product_params2 = [
+  {name: "パセリ", price: 100},
+  {name: "きゅうり", price: 200}
+]
+
+
+
+# 八百屋の開店
+greengrocer1 = Greengrocer.new(product_params1)
+greengrocer2 = Greengrocer.new(product_params2)
+# 商品を表示
+greengrocer1.disp_products
+greengrocer2.disp_products
