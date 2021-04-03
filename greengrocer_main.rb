da846b021267b1642af2b9372f8f3ccd3c6a7e63
itstoasted-0 <snow.w686@gmail.com>
2021-04-03 15:07:23 +0900

    part3 商品を選ぶ

--- greengrocer_main.rb
+++ greengrocer_main.rb
@@ -1,14 +1,18 @@
 class Product
   attr_reader :id, :name, :price
   @@count = 30
+
   def initialize(product_params)
     @id = @@count += 1
     @name = product_params[:name]
     @price = product_params[:price]
   end
+  
 end
 
 class Greengrocer
+  attr_reader :products
+
   def initialize(product_params)
     @products = []
     register_product(product_params)
@@ -27,6 +31,21 @@ class Greengrocer
       puts "#{product.id}.#{product.name}(#{product.price}円)"
     end
   end
+
+end
+
+class User
+
+  def choose_product(products)
+    while true
+      print "商品の番号を選択 > "
+      select_product_id = gets.to_i
+      @choose_product = products.find{|product| product.id == select_product_id}
+      break if !@choose_product.nil?
+      puts "#{products.first.id}から#{products.last.id}番号から選んで下さい。"
+    end
+  end
+
 end
 
 # 商品データ
@@ -46,8 +65,14 @@ adding_product_params1 = [
   {name: "れんこん", price: 350}
 ]
 
-# 商品を登録するメソッドを呼び出す（★ここを追加★）
+# 商品を登録するメソッドを呼び出す
 greengrocer1.register_product(adding_product_params1)
+
+# お客さんの来店
+user = User.new
 
 # 商品を表示
 greengrocer1.disp_products
+
+# (2)商品を選択するメソッドを呼び出し（実引数を設定）
+user.choose_product(greengrocer1.products)
